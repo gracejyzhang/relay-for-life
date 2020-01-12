1ec32f7191405f018fcbb8620812ee9ed8468f61
Grace <unliszt@gmail.com>
2020-01-11 20:55:48 -0500

    Changed route and updated database

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
+from sqlalchemy import desc
+from app.models import Event
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def events():
+    events = Event.query.order_by(desc(Event.timestamp)).all()
+    return render_template('home.html', events=events)
