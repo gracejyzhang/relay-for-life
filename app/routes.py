c979367a3cbcebcdcfb573047f9f0d2f2e435876
3ml12T <55603697+3ml12T@users.noreply.github.com>
2020-01-11 22:49:20 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,21 @@ from flask import render_template
 from app import app
 from sqlalchemy import desc
 from app.models import Event
+from app.forms import RegistrationForm, LoginForm 
+
 
 @app.route('/')
 @app.route('/home')
 def events():
     events = Event.query.order_by(desc(Event.timestamp)).all()
     return render_template('home.html', events=events)
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
