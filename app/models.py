37d96eea07f12d1dafab561f7fa7f463ddfe9071
Grace <unliszt@gmail.com>
2020-01-11 23:26:22 -0500

    Changed events page

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    messages = db.relationship('Message', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -15,7 +16,17 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), index=True, unique=True)
     timestamp = db.Column(db.DateTime, index=True)
+    location = db.Column(db.String(200), index=True)
+    description = db.Column(db.String(500), index=True)
 
     def __repr__(self):
         return '<Post {}>'.format(self.title)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), index=True, unique=True)
+    timestamp = db.Column(db.DateTime, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
 
