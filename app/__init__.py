560eb9da336eccb2d8c71d6dc20c69fd906886df
3ml12T <55603697+3ml12T@users.noreply.github.com>
2020-01-11 22:52:10 -0500

    Update init with Bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import Config
 from flask_migrate import Migrate
-from app.models import db
+from app.models import d
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '95e01697dd1d1b4e543ae883d7fac988'
+bcrypt = Bcrypt(app)
 app.config.from_object(Config)
 db.init_app(app)
 
